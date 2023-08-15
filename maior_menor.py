61d4dc3b254a3a09eaebb8cc1b50c5ef330247a5
leemoraes1 <tad-leticiamoraes@ucpparana.edu.br>
2023-08-15 16:13:26 -0300

    Atividade 3

---
+++ maior_menor.py
@@ -0,0 +1,9 @@
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+if valor1 > valor2:
+    maior_valor = valor1
+else:
+    maior_valor = valor2
+
+print(f"O maior valor é: {maior_valor}")
